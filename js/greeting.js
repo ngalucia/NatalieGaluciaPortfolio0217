fc97be512fda94342e165088d4ff2d9bdcc09f66
Natalie Galucia <NatalieGalucia@Natalies-MacBook-Air-3.local>
2017-03-07 21:11:28 -0600

    Add Complex Data Types to greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -1,10 +1,25 @@
+var welcome = document.querySelector( "#prompt" );
+var goodbye = document.querySelector( "#output" );
+var responses = {
+    "emptyInput": "<p>please <span class='style'>input</span> a string</p>",
+    "correctName": "Welcome to your website!",
+    "incorrectName": "<p>Please enter the <span class='emphasis'>CORRECT</span>name!</p>"
+
+};
+
 var nameTest = function nameTest( name ){
     if( !name ){
-        alert( "please input a string!" );
+        welcome.innerHTML = responses.emptyInput;
     }
     else if( name === "Natalie" ){
-        alert( "Welcome to your website!" );
+        goodbye.textContent =  responses.correctName;
+    }
+    else{
+        welcome.innerHTML = responses.incorrectName;
     }
 };
+
+console.log( welcome.textContent );
+console.log( goodbye.textContent );
 
 nameTest( prompt( "What's your name?" ) );
